32d6c8587fe0d9768640cfcbbe33f6575123c431
Darcey Grundy <grundy.darcey@gmail.com>
2021-08-06 13:12:28 -0400

    accurate table information added

--- seeds/seed.scavenger_hunt.sql
+++ seeds/seed.scavenger_hunt.sql
@@ -1,6 +1,21 @@
-INSERT INTO scavenger 
-  (question, answer)
-VALUES
-  ('What color is the sky?', 'Blue'),
-  ('What''s the opposite of up?', 'Down'),
-  ('What is the absence of heat called?', 'Cold');
+-- INSERT INTO scavenger 
+--   (question, answer)
+-- VALUES
+--   ('What color is the sky?', 'Blue'),
+--   ('What''s the opposite of up?', 'Down'),
+--   ('What is the absence of heat called?', 'Cold');
+
+
+  INSERT INTO scavenger
+    (question, answer)
+  VALUES
+    ('question 1', '1931'),
+    ('question 2', 'Montreal'),
+    ('question 3', 'Eagle'),
+    ('question 4', '1776'),
+    ('question 5', 'Nicolas Cage'),
+    ('question 6', '1976'),
+    ('question 7', '119'),
+    ('question 8', 'South'),
+    ('question 9', 'Palmetto'),
+    ('question 10', 'Viper');
